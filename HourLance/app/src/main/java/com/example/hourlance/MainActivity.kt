a1ceb81ecb7c97a2dd415376cb9def53318caa15
Benjamin Kindle <benjcskindle@gmail.com>
2021-09-05 18:01:02 -0700

    MINOR: Changed some implementation to the nav menu

--- HourLance/app/src/main/java/com/example/hourlance/MainActivity.kt
+++ HourLance/app/src/main/java/com/example/hourlance/MainActivity.kt
@@ -9,18 +9,46 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.hourlance.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    // MY VAR
+    private lateinit var toggle : ActionBarDrawerToggle
+    //
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // MY VARS
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_dashboard -> Toast.makeText(applicationContext, "Clicked Dashboard", Toast.LENGTH_SHORT).show()
+                R.id.nav_clients -> Toast.makeText(applicationContext, "Clicked Clients", Toast.LENGTH_SHORT).show()
+                R.id.nav_invoices -> Toast.makeText(applicationContext, "Clicked Invoices", Toast.LENGTH_SHORT).show()
+                R.id.nav_calendar -> Toast.makeText(applicationContext, "Clicked Calendar", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked Settings", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+        //
 
         setSupportActionBar(binding.toolbar)
 
@@ -44,6 +72,11 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        // MY CODE
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        //
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
