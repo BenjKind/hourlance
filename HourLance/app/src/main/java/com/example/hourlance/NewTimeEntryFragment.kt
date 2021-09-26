7aa1b3df0f2a8ff53954c5ee5db6f6012cf45aa9
Benjamin Kindle <benjcskindle@gmail.com>
2021-09-26 14:00:49 -0700

    Small Tweaks - Getting DatePicker to Function

--- HourLance/app/src/main/java/com/example/hourlance/NewTimeEntryFragment.kt
+++ HourLance/app/src/main/java/com/example/hourlance/NewTimeEntryFragment.kt
@@ -2,18 +2,19 @@ package com.example.hourlance
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.hourlance.databinding.FragmentNewTimeEntryBinding
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
+import com.example.hourlance.MainActivity
 
 
 class NewTimeEntryFragment : Fragment() {
@@ -21,30 +22,34 @@ class NewTimeEntryFragment : Fragment() {
     private val binding get() = _binding!!
 
     // USING https://www.tutorialkart.com/kotlin-android/android-datepicker-kotlin-example/
-    var button_date: Button? = null
+    lateinit var button_date: Button//? = null
     var cal = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        button_date = binding.buttonSelectDate
+        button_date!!.text = "--/--/----"
         // UPDATE BUTTON TO DATE AFTER SETTING
-        val dateSetListener =
-            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
                 cal.set(Calendar.YEAR, year)
-                cal.set(Calendar.MONTH, monthOfYear)
-                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                cal.set(Calendar.MONTH, month)
+                cal.set(Calendar.DAY_OF_MONTH, day)
                 updateDateInView()
             }
-
-        button_date!!.setOnClickListener {
-            DatePickerDialog(
-                this@MainActivity,
-                dateSetListener,
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)
-            ).show()
         }
+
+        button_date.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                DatePickerDialog(v.context,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        })
     }
 
     private fun updateDateInView() {
@@ -57,14 +62,6 @@ class NewTimeEntryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val dateSetListener =
-            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-                cal.set(Calendar.YEAR, year)
-                cal.set(Calendar.MONTH, month)
-                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-                updateDateInView()
-            }
-
         _binding = FragmentNewTimeEntryBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -95,6 +92,14 @@ class NewTimeEntryFragment : Fragment() {
                     "PLEASE ENTER VALID DATA",
                     Toast.LENGTH_SHORT).show()
         }
+
+        /*binding.buttonSelectDate.setOnClickListener {
+            if(binding.datePicker.visibility == View.INVISIBLE) {
+                binding.datePicker.visibility = View.VISIBLE
+            } else {
+                binding.datePicker.visibility = View.INVISIBLE
+            }
+        }*/
     }
 
     private fun saveTimeEntryToFile(_timeEntry: TimeEntryClass): Boolean {
